a0dd3d614341a64c086007b3fb48fb17b47b8f35
Mdidu <alexandre.meddas@gmail.com>
2021-06-07 09:29:25 +0200

    first commit

---
+++ src/main/java/com/signette/SignetteApplication.java
@@ -0,0 +1,13 @@
+package com.signette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignetteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignetteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/signette/SignetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignetteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
