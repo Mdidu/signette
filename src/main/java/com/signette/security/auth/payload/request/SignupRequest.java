a9d135f494d9c04f8fcf15e0973446af51bdb2b1
Mdidu <alexandre.meddas@gmail.com>
2021-06-10 08:58:22 +0200

    Debug signup

--- src/main/java/com/signette/security/auth/payload/request/SignupRequest.java
+++ src/main/java/com/signette/security/auth/payload/request/SignupRequest.java
@@ -1,10 +1,26 @@
 package com.signette.security.auth.payload.request;
 
+import java.util.Date;
+
 public class SignupRequest {
 
     private String mail;
 
     private String password;
+
+    private Date userDateOfBirth;
+
+    private Date userEntryDate;
+
+    private String userLastname;
+
+    private String userName;
+
+    private int userNss;
+
+    private String userPhone;
+
+    private String userUsername;
 
     private int addressId;
 
@@ -40,5 +56,61 @@ public class SignupRequest {
 
     public void setRoleId(int roleId) {
         this.roleId = roleId;
+    }
+
+    public Date getUserDateOfBirth() {
+        return userDateOfBirth;
+    }
+
+    public void setUserDateOfBirth(Date userDateOfBirth) {
+        this.userDateOfBirth = userDateOfBirth;
+    }
+
+    public Date getUserEntryDate() {
+        return userEntryDate;
+    }
+
+    public void setUserEntryDate(Date userEntryDate) {
+        this.userEntryDate = userEntryDate;
+    }
+
+    public String getUserLastname() {
+        return userLastname;
+    }
+
+    public void setUserLastname(String userLastname) {
+        this.userLastname = userLastname;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserNss() {
+        return userNss;
+    }
+
+    public void setUserNss(int userNss) {
+        this.userNss = userNss;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserUsername() {
+        return userUsername;
+    }
+
+    public void setUserUsername(String userUsername) {
+        this.userUsername = userUsername;
     }
 }
